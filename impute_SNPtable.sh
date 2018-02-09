567f36bed8cd8b78c021dab1b3a451b8f2602d86
greensii <greensi@stanford.edu>
2018-02-09 13:34:36 -0800

    added functionality to count alleles if <snptable>.alleleCts does not already exist

--- impute_SNPtable.sh
+++ impute_SNPtable.sh
@@ -4,6 +4,13 @@
 
 snpFile=${1}
 nofFounders=$(( $(head -1 $snpFile | tr ',' ' ' | wc -w) - 2 ))
+
+
+if [ ! -f ${snps}.alleleCts ]; then
+	echo "counting alleles in $snps"; $scriptsDir/count_SNPtable.sh $snps	
+fi
+
+
 posCol=$(head -1 $snpFile.alleleCts | tr ',' '\n' | awk '($1=="pos"){print NR}')
 refCtCol=$(head -1 $snpFile.alleleCts | tr ',' '\n' | awk '($1=="refCt"){print NR}')
 altCtCol=$(head -1 $snpFile.alleleCts | tr ',' '\n' | awk '($1=="altCt"){print NR}')
@@ -11,6 +18,7 @@ altCol=$(head -1 $snpFile.alleleCts | tr ',' '\n' | awk '($1=="alt"){print NR}')
 namesRow=$(head $snpFile | grep -n Ref | cut -f1 -d':')
 
 head -$namesRow $snpFile | tail -1 > $snpFile.imputed
+
 
 
  join -j 1 <(tail -n +2 $snpFile.alleleCts | awk -v cols="$posCol,$refCtCol,$altCtCol,$altCol" -F ',' 'BEGIN{split(cols,colArr,",")}{print $colArr[1]"\t"$colArr[2]"\t"$colArr[3]"\t"$colArr[4]}'  ) <(tail -n +$(( 1 + $namesRow )) $snpFile  | tr ',' '\t' ) |   awk -F ' ' -v nofFounders="$nofFounders" '
