ad5f43b66c1a80683c31be9002595f31333e7e9d
greensii <greensi@stanford.edu>
2018-02-09 12:50:54 -0800

    added impute_SNPtable.sh
    performs simple imputation by randomly assigning alleles to missing calls, weighted by frequency of alt and ref alleles at the same site in called lines

---
+++ impute_SNPtable.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+### IMPUTE ALLELES IN SNP FILE
+
+snpFile=${1}
+nofFounders=$(( $(head -1 $snpFile | tr ',' ' ' | wc -w) - 2 ))
+posCol=$(head -1 $snpFile.alleleCts | tr ',' '\n' | awk '($1=="pos"){print NR}')
+refCtCol=$(head -1 $snpFile.alleleCts | tr ',' '\n' | awk '($1=="refCt"){print NR}')
+altCtCol=$(head -1 $snpFile.alleleCts | tr ',' '\n' | awk '($1=="altCt"){print NR}')
+altCol=$(head -1 $snpFile.alleleCts | tr ',' '\n' | awk '($1=="alt"){print NR}')
+namesRow=$(head $snpFile | grep -n Ref | cut -f1 -d':')
+
+head -$namesRow $snpFile | tail -1 > $snpFile.imputed
+
+
+ join -j 1 <(tail -n +2 $snpFile.alleleCts | awk -v cols="$posCol,$refCtCol,$altCtCol,$altCol" -F ',' 'BEGIN{split(cols,colArr,",")}{print $colArr[1]"\t"$colArr[2]"\t"$colArr[3]"\t"$colArr[4]}'  ) <(tail -n +$(( 1 + $namesRow )) $snpFile  | tr ',' '\t' ) |   awk -F ' ' -v nofFounders="$nofFounders" '
+ BEGIN{ srand()}
+ { 
+ 	pos=$1; refCt=$2; altCt=$3; alt=$4; ref=$5
+ 	if (refCt+altCt>0){
+	printf pos","ref
+	for (i=6;i<=NF;i++) {
+ 		if ($i=="N")
+ 		{	if ( rand() <= altCt/(refCt+altCt))
+ 			{ printf ","alt }
+ 			else {printf ","ref }
+ 		}
+ 		else {printf ","$i}
+ 	}
+ 	printf"\n"
+	}
+} '  >> $snpFile.imputed
+echo "imputed SNPs written to 
+${snpFile}.imputed"
