c075317e21b0ef33977969a98b97caac8a95e26a
sharon <sharongreenblum@Sharons-MacBook.local>
2019-10-23 11:55:03 -0700

    Update numeric_SNPtable.R

--- numeric_SNPtable.R
+++ numeric_SNPtable.R
@@ -4,8 +4,7 @@ args<-commandArgs(TRUE)
 library(data.table)
 
 ## FILES
-snpFile <- args[1] #snpFile<-"/mnt/cages/ref_data/dgrp/snps/all_lines/new_2L.csv"
-#snpInfoFile <- args[2] #snpInfoFile<-"/mnt/cages/ref_data/dgrp/snps/all_lines/new_2L.csv.altCt" 
+snpFile <- args[1] 
 snpInfoFile <- paste0(snpFile,".alleleCts")
 ###### snpInfoFile must be in the same row-order as snpFile, and have a column for identity of the alt allele
 
@@ -15,11 +14,12 @@ snpInfo<- as.data.frame(fread(snpInfoFile))
 chrom<-colnames(SNPs)[1]
 
 ##MAKE NUMERIC SNP TABLE
-### ref=0; alt=1; het=.5; missing=-1
+### CODES: ref=0; alt=1; het=.5; missing=-1
+
 make_numeric=function(x){-0.5*(as.numeric(x==snpInfo$ref)) + 0.5*as.numeric(x==snpInfo$alt) + -1.5*as.numeric(x=="N") + .5}
 numericSNPs<-apply(SNPs[,3:dim(SNPs)[2]],2,make_numeric)
 
-#numericSNPs<-apply(SNPs[,3:dim(SNPs)[2]],2,function(x) {.5*(as.numeric(x=="N")) + as.numeric(x==snpInfo$alt))
+### OLD: numericSNPs<-apply(SNPs[,3:dim(SNPs)[2]],2,function(x) {.5*(as.numeric(x=="N")) + as.numeric(x==snpInfo$alt))
 rownames(numericSNPs)<-SNPs[,1]
 headers<-paste(c(chrom,colnames(numericSNPs)),collapse=",")
 cat(paste(headers,'\n',sep=""),file=paste(snpFile,".numeric",sep=""))
