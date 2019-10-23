2c12c79cc2c9e00a8995a617546ff94061ae3492
sharon <sharongreenblum@Sharons-MacBook.local>
2019-10-23 12:30:34 -0700

    fixed syntax

--- prepare_SNPtable_for_HAFcalc.sh
+++ prepare_SNPtable_for_HAFcalc.sh
@@ -18,11 +18,21 @@ echo "bgzipping ${snptable}.numeric"
 chrom=$(head -1 $snptable | cut -f1 -d',')
 tail -n +2 ${snptable}.numeric | tr ',' '\t' | awk -v chrom="$chrom" '{
 		alleleCt=NF-1
-		printf("%s\t%s",chrom,$1 ); $1="";
+		printf("%s\t%s",chrom,$1 ); 
+		$1="";
 		altCt=0
-		for(ii=2;ii<=NF;ii++){if($ii>=0){altCt=altCt+$ii;calledCt=calledCt+1}}
+		for(ii=2;ii<=NF;ii++){
+			if($ii>=0){
+				altCt=altCt+$ii;
+				calledCt=calledCt+1
+			}
+		}
 		calledRate=altCt/calledCt;  
-		for(ii=2;ii<=NF;ii++){if($ii<0){$ii=calledRate}
+		for(ii=2;ii<=NF;ii++){
+			if($ii<0){
+				$ii=calledRate
+			}
+		}
 		print
 	}' | bgzip > ${snptable}.numeric.bgz
 tabix -s 1 -b 2 -e 2 ${snptable}.numeric.bgz
