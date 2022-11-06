6b965cfa88745a22653889dff2efa5442ac3fe07
Lucas Czech <luc.czech@gmail.com>
2022-11-06 00:05:17 -0700

    Create allele count and numeric tables for imputed tables immediately
    This solves https://github.com/petrov-lab/HAFpipe-line/issues/5

--- scripts/npute_SNPtable.sh
+++ scripts/npute_SNPtable.sh
@@ -21,10 +21,12 @@ fi
 #      Main
 # ==================================================================================================
 
+maindir=$(dirname "$0")/..
+
 snptable=${1}
 winsize=${2:-20}
 mode=${3:-0}
-outfile=${4:-$snptable.npute}
+outfile=${4:-${snptable}.npute}
 nputedir=${5:-"$(dirname $0)/../NPUTEv1"}
 
 tail -n +2 ${snptable} | cut -f3- -d',' | tr 'N' '?' > ${snptable}.nputeIN
@@ -35,7 +37,25 @@ if [ "$mode" = 1 ]; then
 else
     echo "running npute with window size $winsize"
     python $nputedir/NPUTE.py -m $mode -w $winsize -i ${snptable}.nputeIN -o ${snptable}.nputeOUT
-    head -1 $snptable > ${snptable}.npute
+    head -1 ${snptable} > ${snptable}.npute
     paste <(tail -n +2 ${snptable} | cut -f1-2 -d',') <(cat ${snptable}.nputeOUT | tr 'a-z' 'A-Z' | tr '\t' ',') | tr '\t' ',' >> ${snptable}.npute
     rm ${snptable}.nputeIN ${snptable}.nputeOUT
+fi
+
+# We want to create the numeric table now, so that this is done before we move
+# on to Tasks 3 and 4. If running these tasks in parallel for many samples,
+# it could otherwise happen that we work with corrupted files, see
+# https://github.com/petrov-lab/HAFpipe-line/issues/5
+if [ ! -e ${snptable}.npute.numeric.bgz ]; then
+    if [ ! -e ${snptable}.npute.alleleCts ]; then
+        echo "counting alleles in ${snptable}.npute"
+        ${maindir}/scripts/count_SNPtable.sh ${snptable}.npute
+    fi
+
+    echo "preparing ${snptable}.npute for allele frequency calculation"
+    ${maindir}/scripts/prepare_SNPtable_for_HAFcalc.sh ${snptable}.npute
+    if [ ! -e ${snptable}.npute.numeric.bgz ]; then
+        echo "creating ${snptable}.npute.numeric failed"
+        exit 1
+    fi
 fi
